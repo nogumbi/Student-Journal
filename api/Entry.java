5297dcdff1af0bc41f2eb047b2ffaab3bca7936c
nogumbi <nogumbi@student.wethinkcode.co.za>
2022-03-20 09:10:59 +0200

    Entry consisting of a single class

---
+++ api/Entry.java
@@ -0,0 +1,36 @@
+public class Entry{
+    private Integer id;
+    private String title;
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static Entry createEntry(String title, String description){
+        Entry entry = new Entry();
+        entry.title = title;
+        entry.description = description;
+        return entry;
+    }
+}
