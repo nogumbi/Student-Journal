c2c3954b310b4cea70f057e939e6f226741370b1
nogumbi <nogumbi@student.wethinkcode.co.za>
2022-03-20 15:41:08 +0200

    APi handler created

--- src/main/java/webApi/EntryApiHandler.java
+++ src/main/java/webApi/EntryApiHandler.java
@@ -1,5 +1,23 @@
 package main.java.webApi;
 
 public class EntryApiHandler {
-    
+    public static final EntryDB db = new TestDatabase(); 
+
+    public static void getAllEntries(Context context){
+        context.json(db.all());
+    }
+
+    public static void getOne(Context context){
+        Integer id = context.pathParamAsClass("id", Interger.class).get();
+        Entry entry = db.get(id);
+        context.json(entry);
+    }
+
+    public static void createEntry(Context context){
+        Entry entry = context.bodyAsClass(Entry.class);
+        Entry newEntry = db.add(entry);
+        context.header("Location", "/entry/" + entry.getId());
+        context.status(HttpCode.CREATED);
+        context.json(newEntry);
+    }
 }
