6459b7eb311a521b8aab68e5555bbdae36c0a241
nogumbi <nogumbi@student.wethinkcode.co.za>
2022-03-21 04:51:21 +0200

    login page added

--- app/lib/model/main.dart
+++ app/lib/model/main.dart
@@ -18,10 +18,25 @@ class MyApp extends StatelessWidget {
         //Navigation to other screens using routes.
         initialRoute: '/',
         routes: {
+          '/': (context) => const LoginPage(),
           '/home': (context) => const HomePage(),
           '/view_old': (context) => const ViewPage(),
           "/add_new": (context) => const AddNewPage(),
         });
   }
 }
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(224, 230, 233, 1.0),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(74, 51, 65, 1.0),
+          title: const Text("Login"),
+        ));
+  }
+}
+
 
